00b5e902571bd8bee19ed94fcbd1d9751629019e
Hulda Lilja <huldah15@ru.is>
2020-09-17 12:50:20 +0200

    changing colors of icons

--- src/components/Education/Schools.js
+++ src/components/Education/Schools.js
@@ -1,14 +1,29 @@
 import HRlogo from "../../assets/media/hr.svg";
 import AUlogo from "../../assets/media/AU.svg";
+import ETHlogo from "../../assets/media/eth.svg"
 import Verzlologo from "../../assets/media/verzlo.svg";
 
 const schools = [
     {
         logoLink: "https://en.ru.is/st/dcs/graduate-study/msc-software-engineering/",
+        logo:ETHlogo,
+        title:"Eidgenössische Technische Hochschule Zürich",
+        period : "2020-: MSc in Computer Science" ,
+        extraInfo: "MSc at ETH Zürich.",
+    },
+    {
+        logoLink: "https://en.ru.is/st/dcs/graduate-study/msc-software-engineering/",
         logo:HRlogo,
         title:"Reykjavík University",
-        period : "2018-: Part-time MSc in Software Engineering." ,
+        period : "2018-: Part-time MSc in Software Engineering. Not finished." ,
         extraInfo: "Part-time studies while working as a research assistant.",
+    },
+    {
+        logoLink:"https://en.ru.is/st/dcs/undergraduate-study/bsc-software-engineering/",
+        logo:HRlogo,
+        title:"Reykjavík University",
+        period : "2015-2018: BSc in Software Engineering." ,
+        extraInfo: "New-students’ scholarship when I started. After the second and fourth semesters I was a dean's list recipient."
     },
     {
         logoLink:"https://ingenioer.au.dk/en/education/international-programme/software-engineering/",
@@ -17,13 +32,6 @@ const schools = [
         title:"Aarhus University",
         period : "2017-2018: One semester exchange studies during BSc studies." ,
         extraInfo: "International programme for Software Engineering.",
-    },
-    {
-        logoLink:"https://en.ru.is/st/dcs/undergraduate-study/bsc-software-engineering/",
-        logo:HRlogo,
-        title:"Reykjavík University",
-        period : "2015-2018: BSc in Software Engineering." ,
-        extraInfo: "New-students’ scholarship when I started. After the second and fourth semesters I was a dean's list recipient."
     },
     {
         logoLink:"https://www.verslo.is/english/about-us/",
