2adc69cf5a54a7db3e23556f166fe0aefcc26a73
Hulda Lilja <huldah15@ru.is>
2021-09-04 01:01:53 +0200

    adding Google experience and make cards higher.

--- src/components/Experience/works.jsx
+++ src/components/Experience/works.jsx
@@ -3,17 +3,26 @@ import Mimirlogo from "../../assets/media/mimir.svg";
 import NPAlogo from "../../assets/media/npa.svg";
 import Brokeylogo from "../../assets/media/brokey.svg";
 import Reglalogo from "../../assets/media/regla.svg";
+import Googlelogo from "../../assets/media/googlelogo.png";
 import HRDatawelllogo from "../../assets/media/hrdatawell.svg";
 import React from 'react'
 
 const works = [
+    {
+        logo:Googlelogo,
+        companyName:"Google",
+        companyLink:"https://www.google.com",
+        period:"June 2021 - September 2021" ,
+        description:"Site Reliability Engineering Intern",
+        back:(<p>Worked within privacy, safety and security. Used Python, Java and Google speciﬁc technologies for building, testing and deploying. Project included revamping how conﬁgurations are managed and deployed.</p>)
+    },
     {
         logo:Reglalogo,
         companyName:"Regla ehf/ Fakta ehf",
         companyLink:"https://www.regla.is/is/vorur/verkbokhald/",
         period:"Summers of 2017 and 2018, part-time 2018-2020" ,
         description:"Cross-platform and web developer",
-        back:(<p>Designed and developed the company’s first cross-platform mobile app(Xamarin Forms), available on android and iOS. Made a responsive website(ASP.NET) with some of the company’s features as well as new ones that required adopting all layers.</p>)
+        back:(<p>Designed and developed the company’s first cross-platform mobile app(Xamarin Forms), available on android and iOS. Made a responsive website(ASP.NET) with a better acces to some of the company’s features as well as new ones.</p>)
     },
     {
         logo:HRDatawelllogo, 
